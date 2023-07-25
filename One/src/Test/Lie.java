4c7771741e6ceddd1d2400ead81551ae6a7f9e65
4D-Praveen <122081819+4D-Praveen@users.noreply.github.com>
2023-07-25 08:55:37 -0400

    test

--- One/src/Test/Lie.java
+++ One/src/Test/Lie.java
@@ -3,6 +3,8 @@ package Test;
 public class Lie {
 	private void syso() {
 		 System.out.println("valid");
+		 System.out.println("avalid");
+			System.out.println("bvalid");System.out.println("cvalid");System.out.println("dvalid");
 
 	}
 
